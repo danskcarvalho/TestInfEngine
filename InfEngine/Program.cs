19e3c5b37c2f950437a6f2ed867ebabbc361c7fe
Danilo Carvalho <danskcarvalho@gmail.com>
2025-06-24 20:36:08 -0300

    More

--- InfEngine/Program.cs
+++ InfEngine/Program.cs
@@ -8,7 +8,7 @@ var eq = Z.M("Eq");
 var list = Z.A1("List");
 var strEq = Z.Impl0("strEq", str, eq);
 var listEq = Z.Impl1("listEq", t => Z.Impl(list(t), eq, Z.ImplC(t, eq)));
-var goal = Z.ImplG(list(str), eq, "goal1");
+var goal = Z.ImplG(list(list(str)), eq, "goal1");
 var solver = new Solver([goal], [strEq, listEq]);
 var result = solver.Run();
 Console.WriteLine(result);
